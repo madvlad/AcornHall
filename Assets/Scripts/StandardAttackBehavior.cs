c80115f8da3e71827f9d9e517a90ea09d05666ae
David Bohan <madvlad00@googlemail.com>
2018-12-11 23:32:51 -0500

    Add sword swipe control and hitbox
    Issues fixed: #2

---
+++ Assets/Scripts/StandardAttackBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardAttackBehavior : MonoBehaviour {
+
+    public GameObject hitBox;
+    public float attackSustainTime = 0.25f;
+
+	void Start () {
+        hitBox.SetActive(false);
+	}
+	
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            hitBox.SetActive(true);
+            Invoke("DisableWeaponHitbox", attackSustainTime);
+        }
+	}
+
+    private void DisableWeaponHitbox()
+    {
+        this.hitBox.SetActive(false);
+    }
+}
