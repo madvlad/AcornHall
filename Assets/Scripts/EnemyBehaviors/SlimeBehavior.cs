f2a44bea6bedfb282cbad867ecb83db06a91c733
David Bohan <madvlad00@googlemail.com>
2018-12-13 22:46:44 -0500

    Add navmesh to test scene
    Fixes #10

--- Assets/Scripts/EnemyBehaviors/SlimeBehavior.cs
+++ Assets/Scripts/EnemyBehaviors/SlimeBehavior.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// Specific behavior for the Slime Enemy
 /// </summary>
 public class SlimeBehavior : BaseEnemyBehavior {
+    public Transform goal;
 
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+    }
 }
