84b97201105d117fc6aa6090c4d7d4f013a7b2f3
David Bohan <madvlad00@googlemail.com>
2018-12-18 21:05:44 -0500

    Add player action manager
    Also add slime model to slime enemy

---
+++ Assets/Scripts/ObjectBehaviors/ContainerBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerBehavior : MonoBehaviour {
+    public GameObject contents;
+
+    private bool isOpen = false;
+
+	void OnPlayerUse()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            Debug.Log("Got " + contents.name);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviors/StandardActionBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardActionBehavior : MonoBehaviour
+{
+    public BoxCollider actionEffectArea;
+    public float actionSustainTime = 0.25f;
+    public PlayerActionEnum action;
+    public string actionButton;
+
+    void Start()
+    {
+        actionEffectArea.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(actionButton))
+        {
+            actionEffectArea.enabled = true;
+            Invoke("DisableActionEffectArea", actionSustainTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Can not target self for action
+        if (!other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            other.SendMessage(action.ToString(), SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void DisableActionEffectArea()
+    {
+        actionEffectArea.enabled = false;
+    }
+}
